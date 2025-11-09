f2f88f30bb17173efa8d82a59aeca02ca4b884c2
OwenCochell <owencochell@gmail.com>
2025-11-09 01:16:42 -0600

    updated profile step

--- profile.sh
+++ profile.sh
@@ -3,6 +3,6 @@
 # This script will profile a specific command.
 # We just use perf for this...
 
-COM='./main.exe 100 2.269 100000'
+COM='./main.exe 1024 2.269 100000000'
 
 perf record --freq=100 -g ${COM}
